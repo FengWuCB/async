bc016966bcf9da30b3418555088f34427cc9d629
caitinggui <1029645297@qq.com>
2017-12-17 17:36:38 +0800

    增加concurrent.futures说明

--- concurrent_executor.py
+++ concurrent_executor.py
@@ -3,11 +3,33 @@ from concurrent.futures import ThreadPoolExecutor as Pool
 from concurrent.futures import as_completed
 import requests
 
-URLS = ['http://qq.com', 'http://sina.com', 'http://www.baidu.com', ]
+URLS = ['http://qq.com', 'http://sina.com', 'http://www.baidu.com', 'http://c']
 
 
 def task(url, timeout=10):
     return requests.get(url, timeout=timeout)
+
+
+# pool可以被重复使用，而且只要不运行.result()不会抛出异常
+pool = Pool(max_workers=3)
+task1 = pool.submit(task, URLS[0])
+task2 = pool.submit(task, URLS[0])
+# task3的网址是错误的
+task3 = pool.submit(task, URLS[3])
+task4 = pool.submit(task, URLS[0])
+print(task4.result())
+# 在这之前，没有实际调用task3.result(), 没有抛错
+try:
+    task3.result()
+except Exception:
+    print("task3 fail")
+# 就算pool中有task fail，也可以被shutdown
+pool.shutdown()  # 用完之后要自己shutdown，用with的话会系统会自己处理
+try:
+    task5 = pool.submit(task, URLS[0])
+except RuntimeError:
+    print("can't use pool after it was shutdown")
+# import pdb; pdb.set_trace()
 
 
 with Pool(max_workers=3) as executor:
@@ -29,7 +51,7 @@ with Pool(max_workers=3) as executor:
                 except Exception as e:
                     print("func run error: %s" % e)
                 else:
-                print('%s, done, result: %s, %s' % (str(f), f_ret.url, len(f_ret.content)))
+                    print('%s, done, result: %s, %s' % (str(f), f_ret.url, len(f_ret.content)))
         except Exception as e:
             f.cancel()
             print(str(e))
